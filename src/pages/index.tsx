cb635b8e34b47a14b67ec002b3653dbef560ae67
Gustavo Rezende <gustavo.alesalrez@outlook.com>
2025-03-05 20:23:59 -0300

    txtly att 1.0.1

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Txtly - O Editor de Texto Profissional</title>
+        <meta name="description" content="Edite textos de forma simples e avançada com o Txtly." />
+      </Head>
+    </>
+  );
+}
