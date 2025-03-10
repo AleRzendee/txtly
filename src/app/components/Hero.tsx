5e065a9c16008e07cc9539b7e543dc3fe2b8f03d
Gustavo Rezende <gustavo.alesalrez@outlook.com>
2025-03-10 08:29:43 -0300

    txtly 10/03

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { section } from "framer-motion/client";
+import Link from "next/link";
+
+export default function Hero() {
+    return (
+        <section>
+            
+        </section>
+    )
+}
