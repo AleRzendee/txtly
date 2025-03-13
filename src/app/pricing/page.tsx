9f3763efc7249f60249bd3665403541e0e9bf495
Alexandre Rezende <gustavo.alesalrez@outlook.com>
2025-03-13 14:39:32 -0300

    end of 13/03 Afternoon;

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
     title: "Txt.ly ! Planos",
-    description: "Edite e formate textos de forma simples e moderna",
+    description: "Veja os planos disponíveis para você utilizar o Txtly",
     icons: {
         icon: '/images/002-txtly_logo.png',
     }
