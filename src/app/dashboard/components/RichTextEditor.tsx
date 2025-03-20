6cd5bcc0702ae2a8d8c987b2f5d208206d1da1e4
Alexandre Rezende <gustavo.alesalrez@outlook.com>
2025-03-20 20:30:55 -0300

    Tiny att

--- src/app/dashboard/components/RichTextEditor.tsx
+++ src/app/dashboard/components/RichTextEditor.tsx
@@ -1,37 +1,21 @@
-import React, { useState } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
+import { Editor } from "@tinymce/tinymce-react";
 
 const RichTextEditor = () => {
-    const [editorContent, setEditorContent] = useState('');
-
-    const handleEditorChange = (content: string) => {
-        setEditorContent(content);
-    };
-
     return (
-        <div>
-            <h2>Rich Text Editor</h2>
-            <Editor
-                apiKey="ay0q0kysqri0f5kma2h1r8dnjbxkbegpzkadnhymgki9wclf"
-                value={editorContent}
-                onEditorChange={(content) => handleEditorChange(content)}
-                init={{
-                    height: 500,
-                    menubar: true,
-                    plugins: [
-                        'advlist autolink lists link image charmap print preview anchor',
-                        'searchreplace visualblocks code fullscreen',
-                        'insertdatetime media table paste code help wordcount',
-                    ],
-                    toolbar:
-                        'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
-                }}
-            />
-            <div>
-                <h3>Editor Output:</h3>
-                <div dangerouslySetInnerHTML={{ __html: editorContent }} />
-            </div>
-        </div>
+        <Editor
+            apiKey="ay0q0kysqri0f5kma2h1r8dnjbxkbegpzkadnhymgki9wclf"
+            init={{
+                plugins: "searchreplace visualblocks code fullscreen",
+                toolbar:
+                    "undo redo | styleselect | bold italic | alignleft aligncenter alignright | code fullscreen",
+                external_plugins: {
+                    searchreplace: "https://cdn.tiny.cloud/1/no-api-key/tinymce/6/plugins/searchreplace/plugin.min.js",
+                    visualblocks: "https://cdn.tiny.cloud/1/no-api-key/tinymce/6/plugins/visualblocks/plugin.min.js",
+                    code: "https://cdn.tiny.cloud/1/no-api-key/tinymce/6/plugins/code/plugin.min.js",
+                    fullscreen: "https://cdn.tiny.cloud/1/no-api-key/tinymce/6/plugins/fullscreen/plugin.min.js",
+                },
+            }}
+        />
     );
 };
 
